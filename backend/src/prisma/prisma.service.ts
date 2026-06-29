d6e379c741193a703c03d1ac3c25440085cc6cb5
Zainab Wahab <zainabwahab.eth@gmail.com>
2026-06-30 00:41:09 +0100

    updated prisma for render

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
